97d0f88f03598fe2feb0aadae516c024b5a041af
Diego da Silva Santana <diegosantanasantos@yahoo.com.br>
2024-03-05 08:28:01 -0300

    feat: primeiro commit

---
+++ src/test/java/desafioitau/ItauApplicationTests.java
@@ -0,0 +1,13 @@
+package desafioitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
