929343a85a0fdec7c931e5fd2c0b7d357d79e833
Diego da Silva Santana <diegosantanasantos@yahoo.com.br>
2024-03-10 22:01:26 -0300

    test: add teste service

--- src/test/java/com/desafioitau/service/DadosCadastroServiceImplTest.java
+++ src/test/java/com/desafioitau/service/DadosCadastroServiceImplTest.java
@@ -4,6 +4,7 @@ import com.desafioitau.dto.request.DadosCadastroRequestDTO;
 import com.desafioitau.dto.response.DadosCadastroResponseDTO;
 import com.desafioitau.entities.DadosCadastro;
 import com.desafioitau.exceptions.ResourceNotFoundException;
+import com.desafioitau.exceptions.ResourceNotValidException;
 import com.desafioitau.repositories.DadosCadastroRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,10 +13,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,24 +39,62 @@ class DadosCadastroServiceImplTest {
     }
 
     @Test
-    public void cadastrar_QuandoReceberRequisicaoEOCpfNaoExiste_DeveRetornarDadosCadastroResponseDTO() {
+    void cadastrar_QuandoReceberRequisicaoEOCpfNaoExisteNaBaseDados_DeveCadastrarERetornarDadosCadastroResponseDTO() {
         DadosCadastroRequestDTO requestDTO = new DadosCadastroRequestDTO("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
         DadosCadastro cadastro = new DadosCadastro("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
 
         when(cadastroRepository.existsBycpf(requestDTO.getCpf())).thenReturn(false);
         when(cadastroRepository.save(any(DadosCadastro.class))).thenReturn(cadastro);
         DadosCadastroResponseDTO responseDTO = dadosCadastroService.cadastrar(requestDTO);
+
         assertNotNull(responseDTO);
     }
 
-//    @Test
-//    void cadastrar_QuandoReceberRequisicaoEOCpfJaExiste_DeveLancarException() {
-//        DadosCadastroRequestDTO cadastro = new DadosCadastroRequestDTO("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
-//
-//        when(cadastroRepository.existsBycpf(cadastro.getCpf())).thenReturn(true);
-//
-//        assertThrows(ResourceNotFoundException.class, () -> dadosCadastroService.cadastrar(cadastro));
-//    }
+    @Test
+    void listarCadastros_QuandoReceberRequisicao_DeveRetornarListaDeDadosCadastroResponseDTO() {
+        DadosCadastro dadosCadastro1 = new DadosCadastro("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
+        DadosCadastro dadosCadastro2 = new DadosCadastro("Pedro da Silva Santana", "208.941.580-07", 33, "Argentina");
+        when(cadastroRepository.findAll()).thenReturn(Arrays.asList(dadosCadastro1, dadosCadastro2));
+        List<DadosCadastroResponseDTO> responseDTOs = dadosCadastroService.listarCadastros();
+
+        assertNotNull(responseDTOs);
+        assertEquals(2, responseDTOs.size());
+    }
+
+    @Test
+    void cadastrar_QuandoReceberRequisicaoEOCpfJaExiste_DeveLancarException() {
+        DadosCadastroRequestDTO cadastro = new DadosCadastroRequestDTO("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
+
+        when(cadastroRepository.existsBycpf(cadastro.getCpf())).thenReturn(true);
+
+        assertThrows(ResourceNotValidException.class, () -> dadosCadastroService.cadastrar(cadastro));
+    }
+
+    @Test
+    void atualizarDados_QuandoIdExiste_DeveRetornarDadosAtualizados() {
+        Long id = 1L;
+        DadosCadastro dadosCadastro = new DadosCadastro("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
+        DadosCadastroRequestDTO requestDTO = new DadosCadastroRequestDTO("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
+
+        when(cadastroRepository.findById(id)).thenReturn(Optional.of(dadosCadastro));
+        when(cadastroRepository.save(any(DadosCadastro.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        DadosCadastroResponseDTO responseDTO = dadosCadastroService.atualizarDados(id, requestDTO);
+
+        assertNotNull(responseDTO);
+        assertEquals(36, dadosCadastro.getIdade());
+        assertEquals("Brasil", dadosCadastro.getPais());
+    }
+
+    @Test
+    void atualizarDados_QuandoIdNaoExiste_DeveLancarException() {
+        Long id = 1L;
+        DadosCadastroRequestDTO requestDTO = new DadosCadastroRequestDTO("Diego da Silva Santana", "417.701.438-90", 36, "Brasil");
+
+        when(cadastroRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> dadosCadastroService.atualizarDados(id, requestDTO));
+    }
 
     @Test
     void deletarCadastro_QuandoReceberRequisicaoComIdComoParametroENaoExistir_DeveLancarException() {
